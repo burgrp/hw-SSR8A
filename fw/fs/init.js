38d1a38fd3837cd1b8eb2df85d99bc56fec445e5
Pavel Burgr <pb@drake.cz>
2018-09-17 18:09:14 +0200

    WIP

--- fw/fs/init.js
+++ fw/fs/init.js
@@ -19,4 +19,16 @@ let pcfAddress = Cfg.get("ssr8.pcf8574");
 print("PCF8574 address", pcfAddress);
 let pcf = PCF8574.create(pcfAddress, i2c);
 
-Register.add("ssr8", pcf.createRegister(0, 7, 0, false));
+let regsCount = Cfg.get("ssr8.regCount");
+for (let r = 0; r < regsCount; r++) {
+    let cfgPrefix = "ssr8.reg" + chr(48 + r) + ".";
+    let name = Cfg.get(cfgPrefix + "name");
+    let lsb = Cfg.get(cfgPrefix + "lsb");
+    let msb = Cfg.get(cfgPrefix + "msb");
+    let def = Cfg.get(cfgPrefix + "def");
+    let logic = Cfg.get(cfgPrefix + "logic");
+    print("Register", r, name, lsb, msb, def, logic)
+    Register.add(name, pcf.createRegister(lsb, msb, def, !logic));
+}
+
+

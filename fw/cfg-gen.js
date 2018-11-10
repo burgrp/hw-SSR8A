d337277b3f99a9b78dd8a82b4e768020a20796f4
Pavel Burgr <pb@drake.cz>
2018-11-10 01:40:15 +0100

    WIP

--- fw/cfg-gen.js
+++ fw/cfg-gen.js
@@ -3,10 +3,10 @@ for (let c = 0; c < 8; c++) {
 
     console.info(
 `  - ["ssr8.reg${c}", "o", {"title": "Register ${c}"}]
-  - ["ssr8.reg${c}.name", "s", "gpo${c}", {"title": "Register ${c} name"}]
+  - ["ssr8.reg${c}.name", "s", "", {"title": "Register ${c} name"}]
   - ["ssr8.reg${c}.lsb", "i", ${c}, {"title": "Register ${c} lsb"}]
   - ["ssr8.reg${c}.msb", "i", ${c}, {"title": "Register ${c} msb"}]
   - ["ssr8.reg${c}.def", "i", 0, {"title": "Register ${c} default value"}]
-  - ["ssr8.reg${c}.logic", "b", true, {"title": "Register ${c} msb"}]`);  
+  - ["ssr8.reg${c}.logic", "b", true, {"title": "Register ${c} positive or negative logic"}]`);  
 
 }
--- fw/fs/init.js
+++ fw/fs/init.js
@@ -12,24 +12,27 @@ load("api_df_reg_pcf8574.js");
 let i2c = I2C.get();
 
 let ledName = Cfg.get("ssr8.led.name");
-let ledPin = Cfg.get("ssr8.led.pin");
-print("LED", ledName, "at pin", ledPin);
-Register.add(ledName, RegisterGPO.create(ledPin));
+if (ledName) {
+    let ledPin = Cfg.get("ssr8.led.pin");
+    print("LED", ledName, "at pin", ledPin);
+    Register.add(ledName, RegisterGPO.create(ledPin));
+}
 
 let pcfAddress = Cfg.get("ssr8.pcf8574");
 print("PCF8574 address", pcfAddress);
 let pcf = PCF8574.create(pcfAddress, i2c);
 
-let regsCount = Cfg.get("ssr8.regCount");
-for (let r = 0; r < regsCount; r++) {
+for (let r = 0; r < 8; r++) {
     let cfgPrefix = "ssr8.reg" + chr(48 + r) + ".";
     let name = Cfg.get(cfgPrefix + "name");
-    let lsb = Cfg.get(cfgPrefix + "lsb");
-    let msb = Cfg.get(cfgPrefix + "msb");
-    let def = Cfg.get(cfgPrefix + "def");
-    let logic = Cfg.get(cfgPrefix + "logic");
-    print("Register", r, name, lsb, msb, def, logic)
-    Register.add(name, pcf.createRegister(lsb, msb, def, !logic));
+    if (name) {
+        let lsb = Cfg.get(cfgPrefix + "lsb");
+        let msb = Cfg.get(cfgPrefix + "msb");
+        let def = Cfg.get(cfgPrefix + "def");
+        let logic = Cfg.get(cfgPrefix + "logic");
+        print("Register", r, name, lsb, msb, def, logic)
+        Register.add(name, pcf.createRegister(lsb, msb, def, !logic), {
+            neco: 123
+        });
+    }
 }
-
-
